048227e0088ba18ae6ff1f27bb096a153f006e31
Robert Kirberich <robert@kirberich.de>
2013-10-12 15:08:41 +0100

    Make context scrollable with arrow keys

--- quotes_page/core/views.py
+++ quotes_page/core/views.py
@@ -5,68 +5,75 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.cache import cache
+from django.utils import simplejson
 
 import _qi as qi
 from core.models import Speaker, Episode, Quote
 
 def main(request, quote_id):
-	to_search = request.GET.get('search') or request.POST.get('search')
+    to_search = request.GET.get('search') or request.POST.get('search')
+    response = 'raw' if request.GET.get('response') == 'raw' else 'html'
 
-	if quote_id:
-		try:
-			quote = Quote.objects.get(pk=quote_id)
-		except:
-			quote = None
-	elif to_search:
-		search_list = to_search.split(" ")
-		quotes = Quote.objects.all()
-		for term in search_list:
-			quotes = quotes.filter(text__icontains=term)
-		quote = quotes[random.randint(0, len(quotes)-1)] if len(quotes) else None
-	else:
-		num_quotes = cache.get('num_quotes')
-		if not num_quotes:
-			num_quotes = Quote.objects.all().count()
-			cache.set('num_quotes', num_quotes, 60*10)
-		quote = Quote.objects.all()[random.randint(0, num_quotes-1)]
+    if quote_id:
+        try:
+            quote = Quote.objects.get(pk=quote_id)
+        except:
+            quote = None
+    elif to_search:
+        search_list = to_search.split(" ")
+        quotes = Quote.objects.all()
+        for term in search_list:
+            quotes = quotes.filter(text__icontains=term)
+        quote = quotes[random.randint(0, len(quotes)-1)] if len(quotes) else None
+    else:
+        num_quotes = cache.get('num_quotes')
+        if not num_quotes:
+            num_quotes = Quote.objects.all().count()
+            cache.set('num_quotes', num_quotes, 60*10)
+        quote = Quote.objects.all()[random.randint(0, num_quotes-1)]
         
-        context_before = quote.get_previous(3)
-        context_after = quote.get_next(3)
+    context_before = quote.get_previous(3)
+    context_after = quote.get_next(3)
 
-        subs = {
-            'quote': quote,
-            'to_search': to_search if quote else '',
-            'context_before': context_before,
-            'context_after': context_after
-        }
-	return render_to_response('main.html', subs, context_instance=RequestContext(request))
+    subs = {
+        'quote': quote,
+        'to_search': to_search if quote else '',
+        'context_before': context_before,
+        'context_after': context_after
+    }
+
+    if response == 'raw':
+        subs['raw'] = True
+        return render_to_response('quote.html', subs, context_instance=RequestContext(request))
+    else:
+        return render_to_response('main.html', subs, context_instance=RequestContext(request))
 
 
 def init(request):
-	episodes = qi.parse_episodes()
-	for episode_name, episode_dict in episodes.items():
-		episode, created = Episode.objects.get_or_create(name=episode_name)
-		if created:
-			episode.description = episode_dict['description']
-			episode.save()
-		else:
-			episode.quote_set.all().delete()
+    episodes = qi.parse_episodes()
+    for episode_name, episode_dict in episodes.items():
+        episode, created = Episode.objects.get_or_create(name=episode_name)
+        if created:
+            episode.description = episode_dict['description']
+            episode.save()
+        else:
+            episode.quote_set.all().delete()
 
-		if not 'transcript' in episode_dict:
-			continue
-		number_lines = len(episode_dict['transcript'])
-		previous_quote = None
-		for line in range(0, number_lines):
-			speaker_name, text = episode_dict['transcript'][line].split(":", 1)
-			speaker = Speaker.objects.get_or_create(name=speaker_name)[0]
+        if not 'transcript' in episode_dict:
+            continue
+        number_lines = len(episode_dict['transcript'])
+        previous_quote = None
+        for line in range(0, number_lines):
+            speaker_name, text = episode_dict['transcript'][line].split(":", 1)
+            speaker = Speaker.objects.get_or_create(name=speaker_name)[0]
 
-			quote = Quote(episode=episode, speaker=speaker, text=text)
-			if previous_quote:
-				quote.previous = previous_quote
-				quote.save()
-				previous_quote.next = quote
-				previous_quote.save()
-			else:
-				quote.save()
-			previous_quote = quote
-	return HttpResponse("ok")
+            quote = Quote(episode=episode, speaker=speaker, text=text)
+            if previous_quote:
+                quote.previous = previous_quote
+                quote.save()
+                previous_quote.next = quote
+                previous_quote.save()
+            else:
+                quote.save()
+            previous_quote = quote
+    return HttpResponse("ok")
---
+++ quotes_page/templates/main.js
@@ -0,0 +1,70 @@
+quoteCache = {};
+
+$(function() {
+	$("#refresh").focus();
+});
+
+$(document).keyup(function(e){
+	var $quote = $(".quote"),
+		$beforeContext = $quote.prevAll("[class|=context]"),
+		$afterContext = $quote.nextAll("[class|=context]"),
+		changed = false;
+
+	if (e.keyCode == 38 && $beforeContext.length > 0) {
+		e.preventDefault();
+		var newContextId = $beforeContext.last().data("prev");
+			newContextClass = $beforeContext.last().attr("class");
+
+		for (var i = $beforeContext.length-1; i >= 1; i--) {
+			$beforeContext.eq(i).attr("class", $beforeContext.eq(i-1).attr("class")); 
+		}
+		$beforeContext.first().removeClass("context-1").addClass("quote");
+
+		for (var i = 0; i < $afterContext.length-1; i++) {
+			$afterContext.eq(i).attr("class", $afterContext.eq(i+1).attr("class")); 
+		}
+		$afterContext.last().remove();
+		changed = "prev";
+	}
+	else if (e.keyCode == 40 && $afterContext.length > 0) { 
+		e.preventDefault();
+		var newContextId = $afterContext.last().data("next");
+			newContextClass = $afterContext.last().attr("class");
+
+		for (var i = $afterContext.length-1; i >= 1; i--) {
+			$afterContext.eq(i).attr("class", $afterContext.eq(i-1).attr("class")); 
+		}
+		$afterContext.first().removeClass("context-1").addClass("quote");
+
+		for (var i = 0; i < $beforeContext.length-1; i++) {
+			$beforeContext.eq(i).attr("class", $beforeContext.eq(i+1).attr("class")); 
+		}
+		$beforeContext.last().remove();
+		changed = "next";
+	}
+
+	if (changed) {
+		$quote.removeClass("quote");
+		$quote.addClass("context-1");
+
+		if (newContextId) {
+			if (newContextId in quoteCache) {
+				if (changed == "prev") {
+					$(".quotes").prepend(quoteCache[newContextId]);
+				} else {
+					$(".quotes").append(quoteCache[newContextId]);
+				}
+				return;
+			}
+			$.get('/qi/'+newContextId+'?response=raw', function(data, textStatus, jqXHR) {
+				var $el = $(data).attr("class", newContextClass);
+				quoteCache[newContextId] = $el;
+				if (changed == "prev") {
+					$(".quotes").prepend($el);
+				} else {
+					$(".quotes").append($el);
+				}
+			});
+		}
+	}
+});
